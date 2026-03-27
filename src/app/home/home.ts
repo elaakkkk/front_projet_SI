4d5aaaa70edd72777c296b564b81dcc904066d81
Elnaz mac <e.shamsedini@gmail.com>
2026-03-27 08:53:32 +0100

    ajoute le map

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
-  imports:[CommonModule, Navbar]
+  imports: [CommonModule, Navbar]
 })
-export class Home {
+export class Home implements AfterViewInit {
 
   stats = [
     { label: 'Lieux touristiques', value: 128 },
@@ -17,4 +17,26 @@ export class Home {
     { label: 'kg CO₂ économisés', value: 1240 }
   ];
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  async ngAfterViewInit(): Promise<void> {
+    if (isPlatformBrowser(this.platformId)) {
+      // Import dynamique côté client seulement
+      const L = await import('leaflet');
+
+      const map = L.map('map').setView([48.103, -1.672], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+      }).addTo(map);
+
+      const icons = {
+        tourisme: L.icon({ iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png', iconSize: [32,32], iconAnchor: [16,32] }),
+        train: L.icon({ iconUrl: 'https://cdn-icons-png.flaticon.com/512/69/69911.png', iconSize: [32,32], iconAnchor: [16,32] }),
+        co2: L.icon({ iconUrl: 'https://cdn-icons-png.flaticon.com/512/616/616408.png', iconSize: [32,32], iconAnchor: [16,32] })
+      };
+
+      L.marker([48.103, -1.672], { icon: icons.tourisme }).addTo(map).bindPopup('Gare de Rennes');
+    }
+  }
 }
