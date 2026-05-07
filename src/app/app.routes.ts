f127221b3d0b942aed20ab75ad2792eed62ced52
Aymane Sahouri <aymane.sahouri@etudiant.univ-rennes.fr>
2026-05-07 09:07:59 +0200

    fix statistiques et itineraire dynamique

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Co2 } from './co2/co2';
 import { Itineraire } from './itineraire/itineraire';
 import { Statistiques } from './statistiques/statistiques';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'co2', component: Co2 },
   { path: 'itineraire', component: Itineraire },
-  { path: 'statistiques', component: Statistiques }
+  { path: 'statistiques', component: Statistiques },
+  { path: 'apercu', component: Statistiques },
 ];
