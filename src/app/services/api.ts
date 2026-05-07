a1c2e3f328a89c038a09a373cefd7769ed61c0d6
Elnaz mac <e.shamsedini@gmail.com>
2026-05-07 11:14:32 +0200

    api

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class Api {
-  private baseUrl = 'http://127.0.0.1:8000';
+  private baseUrl = 'http://148.60.11.118';
 
   constructor(private http: HttpClient) {}
 
